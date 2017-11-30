e5cbc4dda546becb5ea608c86ee69c0fc10ab95b
Gabriel Briones Sayeg <gabriel.briones.sayeg@intel.com>
2017-11-30 08:41:47 -0600

    Bugfix in configurations view
    Signed-off-by: Gabriel Briones Sayeg <gabriel.briones.sayeg@intel.com>

--- settings/views.py
+++ settings/views.py
@@ -26,7 +26,7 @@ def main(request):
             if send_email(request.POST.get('receiver_email'), 'Bienvenido', "Esta direccion de correo es la que se va a usar para recibir notificaciones de la base de datos de Muelles Obrero"):
                 return HttpResponseRedirect(request.get_full_path())
             else:
-                notifications.append(Notification(message="Configuracion de email incorrecta. Use cuenta de gmail y password correctos"), level="danger")
+                notifications.append(Notification(message="Configuracion de email incorrecta. Use cuenta de gmail y password correctos", level="danger"))
         else:
             notifications.append(Notification(message=str(form.errors), level="danger"))
     form = ConfigurationForm()
